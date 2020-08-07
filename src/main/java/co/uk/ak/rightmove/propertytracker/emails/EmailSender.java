edfae06db90745121068582c2cf5708e2ff36f5a
Ashish Khedekar <ashishkhedekar@gmail.com>
2020-08-08 00:44:42 +0100

    Made email recipients configurable

--- src/main/java/co/uk/ak/rightmove/propertytracker/emails/EmailSender.java
+++ src/main/java/co/uk/ak/rightmove/propertytracker/emails/EmailSender.java
@@ -1,9 +1,9 @@
 package co.uk.ak.rightmove.propertytracker.emails;
 
 import co.uk.ak.rightmove.propertytracker.dto.LettingPropertiesTrackingResult;
-import lombok.AllArgsConstructor;
 import org.joda.time.DateTime;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -11,21 +11,25 @@ import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
 
 @Service
-@AllArgsConstructor
 public class EmailSender
 {
-   private final Environment env;
-
    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
 
-   private final JavaMailSender sender;
+   @Value("${right.move.email.notification.recipients}")
+   private String emailNotificationRecipients;
+
+   @Value("${current.environment}")
+   private String currentEnvironment;
+
+   @Autowired
+   private JavaMailSender sender;
 
    public void sendEmail(final LettingPropertiesTrackingResult trackingResult)
    {
       sender.send(mimeMessage -> {
          MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
          message.setFrom("no-reply-rightmove-tracker@gmail.com");
-         message.setTo("send.me.all.emails.here@gmail.com");
+         message.setTo(emailNotificationRecipients);
          message.setSubject(buildSubject());
          message.setText(String.format("Number of properties let [%s]%n Number of new properties [%s] ", trackingResult.getNumberOfPropertiesLet(), trackingResult.getNewProperties()), true);
       });
@@ -33,9 +37,9 @@ public class EmailSender
 
    private String buildSubject()
    {
-      final String environment = env.getProperty("current.environment");
+
       final StringBuilder subject = new StringBuilder();
-      if (environment == null || !environment.equalsIgnoreCase("PRODUCTION"))
+      if (currentEnvironment == null || !currentEnvironment.equalsIgnoreCase("PRODUCTION"))
       {
          subject.append("[TEST] ");
       }
